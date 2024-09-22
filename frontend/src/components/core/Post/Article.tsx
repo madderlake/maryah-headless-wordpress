cb0f3c3e3a30e3577039a0d6841bc46b446aefc5
madderlake <mary@maryahayne.com>
2024-09-22 10:07:01 -0700

    add categories to article

--- frontend/src/components/core/Post/Article.tsx
+++ frontend/src/components/core/Post/Article.tsx
@@ -1,19 +1,28 @@
 import ContentBlock from '@/components/layout/ContentBlock/ContentBlock';
 import type { Post } from '@/app/lib/types/posts/post';
 import { notFound } from 'next/navigation';
+import { Categories } from '@/app/lib/types/posts/category';
 // import './index.css';
 
 const Article = (data: Post) => {
   if (!data) return notFound();
-  const { id, title, slug, date, content, author } = data;
+  const { id, title, slug, date, content, categories, author } = data;
 
   return (
     <article className={`${slug} post-template post-${id}`}>
       <h2 className="mb-8">{title}</h2>
       <ContentBlock content={content} />
       <div className="row  post-footer ">
-        <p>{author.node.name}</p>
-        <p>{date}</p>
+        <span>{author.node.name}</span>
+        <span> | </span>
+        <span>{date}</span>
+        <span> | </span>
+        <span>
+          Categories:
+          {categories?.nodes.map((cat: any) => {
+            return <span className="inline-block mx-2">{cat.name}</span>;
+          })}
+        </span>
       </div>
     </article>
   );
