ae664e659905ba7a0be64244a550a247c0cad9dd
Mary Hayne <mahayne@gmail.com>
2024-10-03 16:13:25 -0700

    updating env api url (#14)

--- frontend/src/app/lib/fetchApi.ts
+++ frontend/src/app/lib/fetchApi.ts
@@ -1,4 +1,4 @@
-const API_URL = `${process.env.WORDPRESS_API_URL}`;
+const API_URL = `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}`;
 
 export const fetchAPI = async <T extends {}>(
   query: string,
