da14d94edee44265073ff77196a9a00b69c5f01a
madderlake <mary@maryahayne.com>
2024-10-03 14:35:48 -0700

    add images

--- frontend/src/components/core/Post/Article.tsx
+++ frontend/src/components/core/Post/Article.tsx
@@ -2,16 +2,24 @@ import ContentBlock from '@/components/layout/ContentBlock/ContentBlock';
 import type { Post } from '@/app/lib/types/posts/post';
 import { notFound } from 'next/navigation';
 import { Categories } from '@/app/lib/types/posts/category';
-// import './index.css';
+import Image from 'next/image';
 
 const Article = (data: Post) => {
   if (!data) return notFound();
-  const { id, title, slug, date, content, categories, author } = data;
+  const { id, title, slug, date, content, categories, featuredImage, author } =
+    data;
+
+  const {
+    node: { sourceUrl },
+  } = featuredImage;
 
   return (
     <article className={`${slug} post-template post-${id}`}>
       <h2 className="mb-8">{title}</h2>
-      <ContentBlock content={content} />
+      <div className="flex gap-5">
+        <Image src={sourceUrl} alt={title} width={300} height={400} />
+        <ContentBlock content={content} />
+      </div>
       <div className="row  post-footer ">
         <span>{author.node.name}</span>
         <span> | </span>
