12ea0bb8c8fc7c667a2cf790a593a33ea7c7fe9b
madderlake <mary@maryahayne.com>
2024-10-04 11:59:51 -0700

    add remote api, fix null featured img

--- frontend/src/components/core/Post/Article.tsx
+++ frontend/src/components/core/Post/Article.tsx
@@ -1,7 +1,6 @@
 import ContentBlock from '@/components/layout/ContentBlock/ContentBlock';
 import type { Post } from '@/app/lib/types/posts/post';
 import { notFound } from 'next/navigation';
-import { Categories } from '@/app/lib/types/posts/category';
 import Image from 'next/image';
 
 const Article = (data: Post) => {
@@ -9,15 +8,15 @@ const Article = (data: Post) => {
   const { id, title, slug, date, content, categories, featuredImage, author } =
     data;
 
-  const {
-    node: { sourceUrl },
-  } = featuredImage;
+  const sourceUrl = featuredImage?.node?.sourceUrl;
 
   return (
     <article className={`${slug} post-template post-${id}`}>
       <h2 className="mb-8">{title}</h2>
       <div className="flex gap-5">
-        <Image src={sourceUrl} alt={title} width={300} height={400} />
+        {sourceUrl && (
+          <Image src={sourceUrl} alt={title} width={300} height={400} />
+        )}
         <ContentBlock content={content} />
       </div>
       <div className="row  post-footer ">
