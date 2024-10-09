2cf18d89de1128f490f8c49efb0e23c58cc8c57a
Mary Hayne <mahayne@gmail.com>
2024-10-09 14:07:45 -0700

    clean up (#31)
    * clean up
    
    * add next sharp

--- frontend/src/components/templates/LoadTemplate.tsx
+++ frontend/src/components/templates/LoadTemplate.tsx
@@ -1,12 +1,11 @@
-import { getAllPostsForHome } from '@/lib/api';
 import { notFound } from 'next/navigation';
 
 import FlexContent from '@/components/templates/FlexContent/FlexContent';
 import Default from '@/components/templates/Default';
 import Blog from '@/components/templates/Blog';
-import { WPPage } from '@/app/lib/types/pages/wp-page';
+import type { WPPage } from '@/app/lib/types/pages/wp-page';
 
-const LoadTemplate = async ({ tmpl, data }: { tmpl: string; data: WPPage }) => {
+const LoadTemplate = ({ tmpl, data }: { tmpl: string; data: WPPage }) => {
   if (!data) return notFound();
 
   switch (tmpl) {
